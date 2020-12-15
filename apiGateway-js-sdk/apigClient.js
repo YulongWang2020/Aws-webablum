319f900318070d554946aca76e704cd142fa05f6
1023299082 <yw3367@columbia.edu>
2020-12-15 11:56:13 -0500

    change api

--- apiGateway-js-sdk/apigClient.js
+++ apiGateway-js-sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://tyhlzw0fxc.execute-api.us-east-1.amazonaws.com/v1';
+    var invokeUrl = 'https://utakj0htaa.execute-api.us-east-1.amazonaws.com/v1';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
